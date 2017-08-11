b7ce88b2ab13c7d95345e81983d4ee4b72b8039c
Stephan Endres <stephan.endres@outlook.com>
2017-08-11 08:06:02 -0600

    First commit, empty project

---
+++ TiendaMascotas/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TiendaMascotas.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TiendaMascotas/Data/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TiendaMascotas.Data
+{
+    public static class Data
+    {
+        //Ejemplo lista de valores
+        //public static List<Estidad> Entidades = new List<Estidad>()
+        //{
+        //    new Entidad()
+        //    {
+        //        variable1 = "12",
+        //        variable2 = "asdasd"
+        //    }
+        //};
+    }
+}
