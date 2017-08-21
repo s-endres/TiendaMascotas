0fcd2cdf6cb905009b3b868acd984e19406e54ad
armando alfonzo <armandoalfonzo98@gmail.com>
2017-08-21 14:41:21 -0600

    added controllers  bill and discount

---
+++ TiendaMascotas/Controllers/BillController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaMascotas.Models;
+
+namespace TiendaMascotas.Controllers
+{
+    public class BillController : Controller
+    {
+        // GET: Bill
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult getBill(int? BillId)
+        {
+            try
+            {
+                if (BillId != null)
+                {
+                    var bill = Data.Data.BillList.Where(p => p.Id == BillId).FirstOrDefault();
+                    if (bill != null)
+                    {
+                        return Json(bill, JsonRequestBehavior.AllowGet);
+                    }
+                }
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            catch
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+    }
+}
---
+++ TiendaMascotas/Controllers/DiscountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaMascotas.Controllers
+{
+    public class DiscountController : Controller
+    {
+        // GET: Discount
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetAllDiscount()
+        {
+            try
+            {
+                return Json(Data.Data.DiscountList, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public JsonResult GetDiscountById(int? BillId)
+        {
+            try
+            {
+                if (BillId != null)
+                {
+                    return Json(Data.Data.DiscountList.Where(p => p.Id == BillId).FirstOrDefault(), JsonRequestBehavior.AllowGet);
+                }
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return Json(false, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
